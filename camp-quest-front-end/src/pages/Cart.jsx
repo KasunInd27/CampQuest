ef6b012aad3e59c2821da24876a3311247cc0cf6
KasunInd27 <kasunindrajith27@gmail.com>
2026-01-13 17:50:01 +0530

    feat: add Google OAuth login to authentication system

--- camp-quest-front-end/src/pages/Cart.jsx
+++ camp-quest-front-end/src/pages/Cart.jsx
@@ -1,4 +1,3 @@
-// pages/Cart.jsx
 import React, {useState} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ShoppingCart, Trash2, Plus, Minus, Calendar, ArrowRight } from 'lucide-react';
